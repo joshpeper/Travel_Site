d45a1c46ce60b80e2f65fec08873510bf39cbe8f
Josh <peper.josh@outlook.com>
2018-07-02 13:32:02 -0500

    added Modal feature

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
