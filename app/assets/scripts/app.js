811ccb3274a780ae70a016a72edcdc69f713146e
Josh <peper.josh@outlook.com>
2018-07-02 08:57:56 -0500

    Added sticky header with bonus smooth scrolling

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
+var stickyHeader = new StickyHeader();
